5ee41ae08df4388b9bcbbd3794db386ee481a499
fstewart <fstewart@nrn.nrcan.gc.ca>
2018-10-26 15:03:35 -0700

    [first try]

---
+++ Hello-World.R
@@ -0,0 +1,36 @@
+#####################
+# Oct 26, 2018
+# Making sure that I can clone and comit R scripts with Git via the terminal
+# doing this as a test
+######################
+setwd("Z:/GitHub/hello-world")
+main.dir<-"Z:/GitHub/"
+
+# in terminal:
+git clone https://github.com/StewartResearch/hello-world.git
+
+# now make some changes
+
+# git commit-m
+# add a git message
+
+# git commit
+# add to repository
+
+# git push
+# push/sync with github.com
+
+# git pull
+# pulls down from github
+
+# git clone
+# clones the git repositories
+
+######################################## the above only works within the directory of each repository
+
+# git add
+
+# git addA
+
+# git
+# tells you all of the git commands
