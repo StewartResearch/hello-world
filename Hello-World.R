f26831d3dc6cc3c69167fd72b38a2540abf611a1
fstewart <fstewart@nrn.nrcan.gc.ca>
2018-11-13 15:44:35 -0800

    [updating info on RStudio/Git/GitKraken comms]

--- Hello-World.R
+++ Hello-World.R
@@ -27,7 +27,7 @@ git clone https://github.com/StewartResearch/hello-world.git
 # clones the git repositories
 
 # git branch
-# check wich branch you are on
+# check which branch you are on
 
 
 ######################################## the above only works within the directory of each repository
@@ -59,6 +59,42 @@ git clone https://github.com/StewartResearch/hello-world.git
 # ALWAYS REMEMBER TO SAVE YOUR FILES IN R/RSTUDIO 
 #  Git will only update what has been saved. 
 
+
+
+########################################################
+# Making sure Git Repositories are properly set up ----
+########################################################
+
+# 1. Go to a selected repository on GitHub.com
+### fork the repository
+### copy the clone/download URL
+
+# 2. If using GitKraken - go to your GitKraken desktop app
+### clone the repository
+#### do this from GitHub
+#### State where you'd like this "local" version of the repostory saved on your machine
+### On the remote tab, state the original repostory you went to in stage #1 on Github, and name this "upstream"
+
+# 3. Go to RStudio
+### Restart R-studio (ctrl-Shift-F10)
+### in the files section, open the folder that you have added to GitKraken (step #2)
+### Select the Git tab to look at the changes you've made
+### goahead, make changes, and commit them
+
+
+#######################################
+# Getting Git installed with RStudio ----
+#######################################
+
+# you need to have Git on your desktop to make git communicate with GitKraken and/or RStudio
+# install Git from the website
+# then in R studio go to Tools Global or Project options
+# select the file of git (git.exe) that you downloaded
+# click apply
+
+# this should get a Git tab on the upper right hand corner of your RStudio
+# this allows you to use this tab rather than cloning, commiting, and pushing RStudio changes
+# to Git via the terminal command lines (phew!)
 
 
 
